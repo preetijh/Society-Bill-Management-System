4d751dffce214576499cf333fd3b11d21afddcf1
preetijh <98639636+preetijh@users.noreply.github.com>
2023-04-29 13:33:14 +0530

    App.component.ts

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'angular_js';
+// }
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+}
